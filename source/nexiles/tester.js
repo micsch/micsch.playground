7023f1d4592b46cc9bae5b0240e12dfa3e08cd3c
michael <michael@192.168.0.197>
2012-02-04 15:53:37 +0100

    Boilerplate

---
+++ source/nexiles/tester.js
@@ -0,0 +1,4 @@
+// in "nexiles/tester.js"
+require([ "dojo/_base/declare", "dijit/_WidgetBase", "dijit/_TemplatedMixin" ], function(declare, _WidgetBase, _TemplatedMixin){
+    return declare([ _WidgetBase, _TemplatedMixin ], {});
+});
--- source/script.js
+++ source/script.js
@@ -1,1 +1,10 @@
-alert(dojo.version);
+/* Author:
+
+*/
+
+
+
+
+
+
+
